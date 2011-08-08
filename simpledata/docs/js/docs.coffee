0ef513dbc5a7e66a6670910bb61af39d421053ce
Mark <mark@okify.com>
2011-08-08 01:57:52 +0100

    Building tree

--- simpledata/docs/js/docs.coffee
+++ simpledata/docs/js/docs.coffee
@@ -2,16 +2,28 @@ user = 'simplefx'
 repo = 'simplefx.github.com'
 contentFolder = 'simpledata/docs/content'
 
+toDashes = (str) -> str.replace(/[\s\.\/]/g, '-')
+
+addNestedNode = (tree) ->
+  parts = tree.path.split('/')
+  id = toDashes(tree.path)
+  selector = '#' + _.map(parts[0...-1], toDashes).join('-')
+  subList = if tree.type is 'tree' then '<ul></ul>' else ''
+  $(selector + ' > ul').append("<li id=\"#{id}\">#{_.last(parts)}#{subList}</li>")
+
 showContentTree = (trees) ->
   for tree in trees
-    $('#navigation').append("<li>#{tree.path}</li>")
+    if tree.path.indexOf('/') is -1
+      $('#navigation').append("<li id=\"#{toDashes(tree.path)}\">#{tree.path}<ul></ul></li>")
+    else
+      addNestedNode tree
 
 getSubTree = (tree, subTreeNames) ->
   subTree = _.first(_.filter(tree, (item) -> item.path is _.first(subTreeNames)))
   if _.size(subTreeNames) is 1
     $.getJSON subTree.url + '?recursive=1&callback=?', (data) -> showContentTree data.data.tree
   else
-    $.getJSON subTree.url + '?callback=?', (data) => getSubTree data.data.tree, _.rest(subTreeNames)
+    $.getJSON subTree.url + '?callback=?', (data) -> getSubTree data.data.tree, _.rest(subTreeNames)
 
 $ ->
   commitsUrl = "https://api.github.com/repos/#{user}/#{repo}/commits?callback=?"
--- simpledata/docs/js/docs.js
+++ simpledata/docs/js/docs.js
@@ -1,15 +1,25 @@
 (function() {
-  var contentFolder, getSubTree, repo, showContentTree, user;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var addNestedNode, contentFolder, getSubTree, repo, showContentTree, toDashes, user;
   user = 'simplefx';
   repo = 'simplefx.github.com';
   contentFolder = 'simpledata/docs/content';
+  toDashes = function(str) {
+    return str.replace(/[\s\.\/]/g, '-');
+  };
+  addNestedNode = function(tree) {
+    var id, parts, selector, subList;
+    parts = tree.path.split('/');
+    id = toDashes(tree.path);
+    selector = '#' + _.map(parts.slice(0, -1), toDashes).join('-');
+    subList = tree.type === 'tree' ? '<ul></ul>' : '';
+    return $(selector + ' > ul').append("<li id=\"" + id + "\">" + (_.last(parts)) + subList + "</li>");
+  };
   showContentTree = function(trees) {
     var tree, _i, _len, _results;
     _results = [];
     for (_i = 0, _len = trees.length; _i < _len; _i++) {
       tree = trees[_i];
-      _results.push($('#navigation').append("<li>" + tree.path + "</li>"));
+      _results.push(tree.path.indexOf('/') === -1 ? $('#navigation').append("<li id=\"" + (toDashes(tree.path)) + "\">" + tree.path + "<ul></ul></li>") : addNestedNode(tree));
     }
     return _results;
   };
@@ -23,9 +33,9 @@
         return showContentTree(data.data.tree);
       });
     } else {
-      return $.getJSON(subTree.url + '?callback=?', __bind(function(data) {
+      return $.getJSON(subTree.url + '?callback=?', function(data) {
         return getSubTree(data.data.tree, _.rest(subTreeNames));
-      }, this));
+      });
     }
   };
   $(function() {
